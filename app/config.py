7b9e8753748d24e699b0839af1d143e603f79284
Md.Abdullah al mamun <mamun1214@gmail.com>
2025-06-24 22:02:20 +0600

    update PDF generation

--- app/config.py
+++ app/config.py
@@ -49,6 +49,11 @@ class Settings:
     BROWSER_LAUNCH_TIMEOUT = int(os.getenv("BROWSER_LAUNCH_TIMEOUT", 120000))  # 2 minutes
     NAVIGATION_TIMEOUT = int(os.getenv("NAVIGATION_TIMEOUT", 60000))  # 1 minute
     PDF_GENERATION_TIMEOUT = int(os.getenv("PDF_GENERATION_TIMEOUT", 60000))  # 1 minute
+    # Resource control
+    MAX_PDF_GENERATION_ATTEMPTS = int(os.getenv("MAX_PDF_GENERATION_ATTEMPTS", 5))
+    PDF_FILENAME_MAX_LENGTH = int(os.getenv("PDF_FILENAME_MAX_LENGTH", 120))
+    # Cleanup Settings
+    PDF_RETENTION_DAYS = int(os.getenv("PDF_RETENTION_DAYS", 7))
 
 
 settings = Settings()
